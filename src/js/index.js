0a33561edc65efecbd66b136a110d4db625007ce
DongrockOh <87560481+DongrockOh@users.noreply.github.com>
2021-11-26 12:55:28 +0900

    #19 localStorage - 메뉴 수정
    data-menu-id="${index}" - HTML에 커스텀 속성을 표시하는데 표준화된 방법ㅂ을 제공하기 위해 제한, 표준 속성처럼 사용 가능
    
    dataset - "data-*" 어트리뷰트로 표기 된것을 불러온다.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 // TODO localStorage Read &  Write
 // localStorage에 데이터를 저장하여 새로고침해도 데이터가 남아있게 한다.
 // - [] localStorage에 데이터를 저장한다.
+// - [x] 메뉴를 추가할 때
+// - [] 메뉴를 수정할 때
+// - [] 메뉴를 삭제할 때
 // - [] localStorage에 있는 데이터를 읽어온다.
 
 // TODO 카테고리별 메뉴판 관리
@@ -52,10 +55,10 @@ function App() {
     this.menu.push({ name: espressoMenuName });
     store.setLocalStorage(this.menu);
     const template = this.menu
-      .map((item) => {
+      .map((menuItem, index) => {
         return `
-      <li class="menu-list-item d-flex items-center py-2">
-    <span class="w-100 pl-2 menu-name">${item.name}</span>
+      <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+    <span class="w-100 pl-2 menu-name">${menuItem.name}</span>
     <button
       type="button"
       class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
@@ -79,8 +82,11 @@ function App() {
 
   // 메뉴 수정 [변수]
   const updateMenuName = (e) => {
+    const menuId = e.target.closest("li").dataset.menuId;
     const $menuName = e.target.closest("li").querySelector(".menu-name");
     const updateMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
+    this.menu[menuId].name = updateMenuName;
+    store.setLocalStorage(this.menu);
     $menuName.innerText = updateMenuName;
   };
 
