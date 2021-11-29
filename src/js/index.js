88e79fc622eb5262f61c45b9eba1c2197866d24f
DongrockOh <87560481+DongrockOh@users.noreply.github.com>
2021-11-29 13:15:19 +0900

    #25 품절 상태 관리
    click 이벤트에 return을 추가한 이유는 밑에 불필요한 연산들을 할 필요가 없으므로.
    
    this = !this -> 이처럼 작성시 toggle처럼 값을 변경해 줄 수 있다.

--- src/js/index.js
+++ src/js/index.js
@@ -21,6 +21,7 @@
 
 // TODO 품절 상태 관리
 // 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
+// - [] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
 // - [] 품절 버튼을 추가한다.
 // - [] 품절 버튼을 클릭하면 LocalStroage에 상태값이 저장된다.
 // - [] 클릭 이벤트에서 가장 가까운 li태그의 class속성 값에 sold-out을 추가한다.
@@ -59,7 +60,15 @@ function App() {
       .map((menuItem, index) => {
         return `
       <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
-    <span class="w-100 pl-2 menu-name">${menuItem.name}</span>
+         <span class="w-100 pl-2 menu-name ${
+           menuItem.soldOut ? "sold-out" : ""
+         } ">${menuItem.name}</span>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+    >
+      품절
+    </button>
     <button
       type="button"
       class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
@@ -119,14 +128,30 @@ function App() {
     }
   };
 
+  // 품절 상태
+  const soldOutMenu = (e) => {
+    const menuId = e.target.closest("li").dataset.menuId;
+    this.menu[this.currentCategory][menuId].soldOut =
+      !this.menu[this.currentCategory][menuId].soldOut;
+    store.setLocalStorage(this.menu);
+    render();
+  };
+
   // 메뉴 수정, 삭제 [클릭 이벤트]
   $("#menu-list").addEventListener("click", (e) => {
     if (e.target.classList.contains("menu-edit-button")) {
       updateMenuName(e);
+      return;
     }
 
     if (e.target.classList.contains("menu-remove-button")) {
       removeMenuName(e);
+      return;
+    }
+
+    if (e.target.classList.contains("menu-sold-out-button")) {
+      soldOutMenu(e);
+      return;
     }
   });
 
